627a7c56f003e4bab3dd4d37491e1bb9d1a959d3
Shivansh Kush <kushivansh27@gmail.com>
2026-05-25 00:30:54 +0530

    update

--- nvim/init.lua
+++ nvim/init.lua
@@ -549,6 +549,11 @@ require("render-markdown").setup({})
 
 --- smear cursor
 vim.pack.add({ "https://github.com/sphamba/smear-cursor.nvim" })
+require("smear_cursor").setup({
+  stiffness = 0.5,
+  trailing_stiffness = 0.5,
+  matrix_pixel_threshold = 0.5,
+})
 
 --- vim sudo
 vim.pack.add({ "https://github.com/lambdalisue/vim-suda" })
